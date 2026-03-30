106a5bc67fe8178822cc67995fcbbcefb421c7a9
MondTech <support@mondtech.in>
2026-03-30 22:40:55 +0530

    netlify poc added

---
+++ app/marketpoc/page.tsx
@@ -0,0 +1,12 @@
+export default function MyProjectPage() {
+  return (
+    <iframe
+      src="/daily-mode.netlify.app/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}

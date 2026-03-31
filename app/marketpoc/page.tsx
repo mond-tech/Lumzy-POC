29bd1ea946df832aa32c90ac35b5abd8043a859e
MondTech <support@mondtech.in>
2026-04-01 00:28:09 +0530

    second poc updated

--- app/marketpoc/page.tsx
+++ app/marketpoc/page.tsx
@@ -1,7 +1,7 @@
 export default function MyProjectPage() {
   return (
     <iframe
-      src="/daily-mode.netlify.app/index.html"
+      src="/daily-mode.netlify.app/new-section/index.html"
       style={{
         width: "100%",
         height: "100vh",
